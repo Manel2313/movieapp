89b87c821838f64b8f8eab015fb6467360487982
Manel2313 <djemal.manel@gmail.com>
2019-05-21 14:57:47 +0100

    Updates

---
+++ precache-manifest.f094c229fcfca549d184b5405a539de8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63f215f77ea14887180fc04bb0fbd7fc",
+    "url": "/movieapp/index.html"
+  },
+  {
+    "revision": "7cb588dca87225b104d4",
+    "url": "/movieapp/static/css/main.bc2ec9b6.chunk.css"
+  },
+  {
+    "revision": "5694e1898ca4c92fd14d",
+    "url": "/movieapp/static/js/2.6408f43b.chunk.js"
+  },
+  {
+    "revision": "7cb588dca87225b104d4",
+    "url": "/movieapp/static/js/main.e52ccf71.chunk.js"
+  },
+  {
+    "revision": "4e19d316896026f2633d",
+    "url": "/movieapp/static/js/runtime~main.839b6e54.js"
+  }
+]);
